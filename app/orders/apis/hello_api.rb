13515a740f58114b3e2fdc90b0012dd17d7d76ed
Ashton Thomas <athomas@bellycard.com>
2015-11-21 12:34:39 -0600

    add some sample code

--- app/orders/apis/hello_api.rb
+++ app/orders/apis/hello_api.rb
@@ -3,7 +3,8 @@ class HelloApi < Grape::API
   resource :hello do
     desc 'Return a Hello World message'
     get do
-      { message: 'Hello Wonderful World, from ChiWinery!' }
+      sample_order = ShowSampleOrder.new("Coffee", 15)
+      { message: sample_order.call }
     end
   end
 
---
+++ app/orders/operations/show_sample_order.rb
@@ -0,0 +1,19 @@
+class ShowSampleOrder
+
+  def initialize(name: 'sample', quantity: 0)
+    @name = name
+    @quantity = quantity
+    change_stuff
+  end
+
+  def call
+    "Here is #{@name} order for a quantity of #{@quantity}"
+  end
+
+  private
+
+  def change_stuff
+    @name = "Cool #{name}"
+  end
+
+end
