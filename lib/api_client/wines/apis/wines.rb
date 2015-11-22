d6cd143d09baab7300dd5488d95ea9e2194f58fc
Ashton Thomas <athomas@bellycard.com>
2015-11-22 09:38:43 -0600

    branch napa and bump grape

--- lib/api_client/wines/apis/wines.rb
+++ lib/api_client/wines/apis/wines.rb
@@ -1,8 +1,17 @@
 module ApiClient
   class Wines
     def self.get
+      env = {}
+      env['rack.input'] = Puma::NullIO.new
+      env['REQUEST_METHOD'] = 'GET'
+      env['REQUEST_PATH'] = '/orders/hello'
+      env['REQUEST_URI'] = '/orders/hello'
+
       binding.pry
-      WinesComponent::Wines.get
+      response = ApplicationApi.call(env)
+
+      # WinesComponent::Wines.call
+
     end
   end
 end
