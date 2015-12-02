1b73d5ea011f3eb01dc70e2c2927dba4d32bae7f
Ashton Thomas <athomas@bellycard.com>
2015-12-02 09:36:25 -0600

    update notes

--- components/wine/api/wines_api.rb
+++ components/wine/api/wines_api.rb
@@ -1,20 +1,17 @@
 module WineComponent
   class WinesApi < Grape::API
 
-    resource :hello do
-      desc 'Return a Hello World message'
-      get do
-        wine_maker = WineMaker.new(name: "RedBlueGreen", vintage: 2015)
-        { message: wine_maker.call }
-      end
-    end
+    # resource :hello do
+    #   desc 'Return a Hello World message'
+    #   get do
+    #     wine_maker = WineMaker.new(name: "RedBlueGreen", vintage: 2015)
+    #     { message: wine_maker.call }
+    #   end
+    # end
 
-    desc 'just get it'
+    desc 'Just get it. Test overriding this during specs'
     get do
-      # represent Wine.all.first, with: WineRepresenter
-      # include Grape::Roar::Representer
       wine = Wine.all.first
-
       wine.extend(WineRepresenter)
     end
 
