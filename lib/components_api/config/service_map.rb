4c33c5f9e4f1885f0211c929da3be56f5b6b6633
Ashton Thomas <athomas@bellycard.com>
2015-12-04 17:52:39 -0600

    notes

--- lib/components_api/config/service_map.rb
+++ lib/components_api/config/service_map.rb
@@ -6,5 +6,7 @@ module ServiceMap
 
   # this shouldn't deal with the entire URL only the app/machine name
   # our proxy should deal with routing to the correct actual machine..??
+  # actually, there is probably a better way with DNS settings - but I'd rather not have another dependency
+  # until I really need to (for like live changes with no deploys, etc)
 
 end
