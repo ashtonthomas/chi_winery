e4ce6d9a157e2ddb330fe3a9c50f007960cf3ce5
Ashton Thomas <athomas@bellycard.com>
2016-02-08 10:17:28 -0500

    just adding a note

--- lib/components_api/wine/api/wine_api.rb
+++ lib/components_api/wine/api/wine_api.rb
@@ -12,5 +12,21 @@ module WineComponent
     register_get :hello, :blah
     register_get :get_foo, :id, :fu, :bar
 
+    # So our clients break HATEOAS as it knows about:
+    # - resource: WineApi
+    # - method: :do_cool
+    # - url_variables: :id, :foo, :bar
+    # This is fine. It let's up valdiate the request when it is called
+    # (so this _can_ get out of sync with the svc-doc, but that would point to a no-no)
+    # API changes should happen slowly
+    # but we could alert immediately if the svc-doc returns with something
+    # incompatible
+    # But what _do_ we get:
+    # -- quietly stop using variables
+    # -- change order or structure of url + variables at will (when moving components or just changing the routing)
+    # -- can determine the time range to support old of new (but optional) variables
+    #
+    # register_get :do_cool, url_variables: [id, :foo, :bar]
+
   end
 end
